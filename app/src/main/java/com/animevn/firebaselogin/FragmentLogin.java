537bac86ca615b1dfcdedbc40d27d00d28476eb2
animevn <haanhth21@gmail.com>
2020-09-13 21:36:48 +0700

    fragment login OK

--- app/src/main/java/com/animevn/firebaselogin/FragmentLogin.java
+++ app/src/main/java/com/animevn/firebaselogin/FragmentLogin.java
@@ -4,19 +4,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import com.google.android.material.textfield.TextInputEditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FragmentLogin extends Fragment {
+
+    @BindView(R.id.editTextEmail)
+    TextInputEditText editTextEmail;
+    @BindView(R.id.editTextPassword)
+    TextInputEditText editTextPassword;
+    @BindView(R.id.buttonLogin)
+    Button buttonLogin;
+    @BindView(R.id.textViewForgotPassword)
+    TextView textViewForgotPassword;
+    @BindView(R.id.checkBoxRememberMe)
+    CheckBox checkBoxRememberMe;
+    @BindView(R.id.textViewGoogleSignIn)
+    TextView textViewGoogleSignIn;
+    @BindView(R.id.textViewRegister)
+    TextView textViewRegister;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() != null){
+            //hide action bar
+            ((MainActivity)getActivity()).hideActionBar();
+
+            //lock orientation to portrait
+            ((MainActivity)getActivity()).setScreenOrientationPortrait();
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, view);
+        handleLoginCheckBox();
+        return view;
+    }
 
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    private void handleLoginCheckBox(){
+        checkBoxRememberMe.setOnCheckedChangeListener((buttonView, isChecked) -> {
+
+        });
+    }
+
+    @OnClick({R.id.buttonLogin, R.id.textViewForgotPassword,
+            R.id.textViewGoogleSignIn, R.id.textViewRegister})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLogin:
+                break;
+            case R.id.textViewForgotPassword:
+                break;
+            case R.id.textViewGoogleSignIn:
+                break;
+            case R.id.textViewRegister:
+                break;
+        }
     }
 }
--- app/src/main/java/com/animevn/firebaselogin/MainActivity.java
+++ app/src/main/java/com/animevn/firebaselogin/MainActivity.java
@@ -1,9 +1,30 @@
 package com.animevn.firebaselogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void hideActionBar(){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+    }
+
+    public void showActionBar(){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().show();
+        }
+    }
+
+    public void setScreenOrientationPortrait(){
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void setScreenOrientationUser(){
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
